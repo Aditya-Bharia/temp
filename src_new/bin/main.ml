26dcc39c253cd4db7644b36331e9ffda5170b140
Aditya-Bharia <adityaravindra4@gmail.com>
2026-03-22 02:20:19 +0530

    added tokenizer and sample dsl file

--- src_new/bin/main.ml
+++ src_new/bin/main.ml
@@ -1,1 +1,8 @@
-let () = print_endline "Hello, World!"
+open Src_new
+open My_utils
+open Tokenizer
+let source_game = read_file "./source-code/abc.agen"
+let () =
+let tokens = tokenize (explode source_game) in
+let tokens_as_str = print_tokens tokens in
+ print_endline tokens_as_str;
